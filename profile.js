0927743fa31017773488ff117a15cc7b16f145a3
Sumeet <90512820+sumeet05xcaliber@users.noreply.github.com>
2023-08-24 18:14:33 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -17,155 +17,7 @@ const firebaseConfig = {
     const day = String(date.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   }
-   
-  const checkCollectionExists = async (collectionName) => {
-    const collectionRef = db.collection(collectionName);
-    const snapshot = await collectionRef.get();
-    return !snapshot.empty;
-  };
-  
-  const displayButtonBasedOnCollection = async (collectionName) => {
-    const submitButton = document.getElementById('submitButton');
-    const updateButton = document.getElementById('updateButton');
-    const collectionExists = await checkCollectionExists(collectionName);
-  
-    if (collectionExists) {
-      submitButton.style.display = 'none';
-      updateButton.style.display = 'block';
-    } else {
-      submitButton.style.display = 'block';
-      updateButton.style.display = 'none';
-    }
-  };
-  const currentDate = new Date();
-  const collectionName = formatDate(currentDate);
-  displayButtonBasedOnCollection(collectionName);
-  
-  // Call this function when the page loads or when a user logs in
-  
-  
-  const Submit = () => {
-  let inputVal = document.getElementById('diary').value;
-  const currentDate = new Date();
-  const collectionName = formatDate(currentDate); // Use this as the collection name
-  const documentId = formatDate(currentDate); // Use this as the document ID
-
-  db.collection(collectionName).doc(documentId).set({
-    data: inputVal,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  }).then(() => {
-    console.log("Document inserted successfully");
-    inputVal = "";
-    displayDiaryEntries(collectionName);
-
-    displayButtonBasedOnCollection(collectionName); // Update the button visibility
-  }).catch((err) => {
-    console.log(err);
-  });
-};
-  
-  
-  // const Update = () => {
-  //   let inputVal = document.getElementById('diary').value;
-  //   const currentDate = new Date();
-  //   const collectionName = formatDate(currentDate);
-  
-  //   // Assume you want to update the latest entry in the collection
-  //   db.collection(collectionName)
-  //     .orderBy("timestamp", "desc")
-  //     .limit(1)
-  //     .get()
-  //     .then((querySnapshot) => {
-  //       querySnapshot.forEach((doc) => {
-  //         db.collection(collectionName).doc(doc.id).update({
-  //           data: inputVal,
-  //           timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  //         }).then(() => {
-  //           console.log("Document updated successfully");
-  //           inputVal = "";
-  //           displayDiaryEntries(collectionName);
-  //         }).catch((err) => {
-  //           console.log(err);
-  //         });
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-  
-  const displayDiaryEntries = (collectionName) => {
-    const diaryEntriesDiv = document.getElementById('diaryEntries');
-  
-    diaryEntriesDiv.innerHTML = ''; // Clear previous entries
-  
-    db.collection(collectionName)
-      .orderBy("timestamp", "desc")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          const entryText = data.data;
-          const timestamp = data.timestamp.toDate().toLocaleString();
-          const userBioElement = document.getElementById('diaryEntries');
-          userBioElement.textContent = `Diary content: ${entryText}`;
-        //   const entryDiv = document.createElement('div');
-        //   entryDiv.innerHTML = `
-        //     <p>${entryText}</p>
-        //     <small>${timestamp}</small>
-        //     <hr>
-        //   `;
-  
-        //   diaryEntriesDiv.appendChild(entryDiv);
-
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  // const fetchDiary=()=>{
-  //   const enterDate=document.getElementById('date').value;
-  //   const selectedDate=new Date(enterDate);
-  //   if(isNaN(selectedDate)){
-  //     console.log("inavid date format");
-  //     return;
-  //   }
-  //   const collectionName=formatDate(selectedDate);
-  //   const diaryEntries=document.getElementById('diaryEntries');
-  //   // db.collection(collectionName).get()
-  //   // .then((querySnapshot)=>{
-  //   //   querySnapshot.forEach((doc)=>{
-  //   //     const data=doc.data();
-  //   //     const entryText=data.data;
-  //   //     diaryEntries.textContent=`Diary Content :${entryText}`
-  //   //   });
-  //   //   if (querySnapshot.empty) {
-  //   //     console.log("No diary entries found for the selected date.");
-  //   //   }
-  //   // })
-  //   // .catch((err) => {
-  //   //   console.log(err);
-  //   // });
-  //   db.collection(collectionName).doc(collectionName)
-  //   .get()
-  //   .then((doc) => {
-  //     if (doc.exists) {
-  //       const data = doc.data();
-  //       const entryText = data.data;
-  //       diaryEntries.textContent=`Diary Content :${entryText}`
-
-  //     } else {
-  //       console.log("No diary entries found for the selected date.");
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  
-    
-  // }
-  const fetchDiary = () => {
+  const fetchOrCreateDiary = () => {
     const enteredDate = document.getElementById('date').value;
     const selectedDate = new Date(enteredDate);
   
@@ -184,12 +36,42 @@ const firebaseConfig = {
           const data = doc.data();
           const entryText = data.data;
           diaryEntriesDiv.textContent = `Diary Content: ${entryText}`;
-          
-          // Fill the textarea with the fetched entry
+  
+          document.getElementById('updateButton').style.display = 'block';
+          document.getElementById('submitButton').style.display = 'none';
+  
           document.getElementById('diary').value = entryText;
         } else {
           diaryEntriesDiv.textContent = "No diary entries found for the selected date.";
+  
+          document.getElementById('submitButton').style.display = 'block';
+          document.getElementById('updateButton').style.display = 'none';
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  
+  const Submit = () => {
+    const inputVal = document.getElementById('diary').value;
+    const enteredDate = document.getElementById('date').value;
+    const selectedDate = new Date(enteredDate);
+  
+    if (isNaN(selectedDate)) {
+      console.log("Invalid date format.");
+      return;
+    }
+  
+    const collectionName = formatDate(selectedDate);
+  
+    db.collection(collectionName).doc(collectionName)
+      .set({
+        data: inputVal,
+      })
+      .then(() => {
+        console.log("Document inserted successfully");
+        fetchOrCreateDiary(); 
       })
       .catch((err) => {
         console.log(err);
@@ -211,16 +93,13 @@ const firebaseConfig = {
     db.collection(collectionName).doc(collectionName)
       .update({
         data: inputVal,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
       })
       .then(() => {
         console.log("Document updated successfully");
-        fetchDiary(); // Fetch and display updated data
+        fetchOrCreateDiary(); 
       })
       .catch((err) => {
         console.log(err);
       });
   };
   
-  // displayDiaryEntries(collectionName);
-
